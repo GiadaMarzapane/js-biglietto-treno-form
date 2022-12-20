2c86efae688295ee424c7c2810e77c29f9765bab
GiadaMarzapane <giada.ortesta@gmail.com>
2022-12-20 20:06:07 +0100

    Impostati valori più campi in pagina

--- js/script.js
+++ js/script.js
@@ -3,25 +3,44 @@ const myKm = document.getElementById('my-km');
 const myAge = document.getElementById('my-age');
 const submitButton = document.getElementById('submit');
 
+const passengerName = document.getElementById('passenger-name');
+const offerType = document.getElementById('offer-type');
+const ticketPrice = document.getElementById('my-price');
+const wagonNumber = document.getElementById('wagon');
+const ticketCodeNumber = document.getElementById('ticket-code');
 
 submitButton.addEventListener('click',
     function(){
         console.log('click sul button submit');
         console.log("L'utente si chiama: ", myNameSurname.value);
+        passengerName.innerHTML = myNameSurname.value;
         console.log('I km da percorrere sono: ', myKm.value);
-        console.log("L'utente ha: ", myAge.value, ' anni');
+        console.log("L'utente è: ", myAge.value);    
         let basePrice = myKm.value * 0.21;
         basePrice = basePrice.toFixed(2);
-        if (myAge.value < 18){
+        let randomWagonNumber = Math.floor((Math.random() * 10) + 1);
+        wagonNumber.innerHTML = randomWagonNumber;
+        let randomTicketCodeNumber = Math.floor((Math.random() * 10000) + 90000);
+        ticketCodeNumber.innerHTML = randomTicketCodeNumber;
+
+        if (myAge.value == 'young'){
             let discountPrice20 = (basePrice - (basePrice * 0.20)).toFixed(2);
             console.log('Prezzo scontato per i minorenni: €', discountPrice20);
+            offerType.innerHTML = 'Offerta Young -20%';
+            ticketPrice.innerHTML = '€ ' + discountPrice20;            
         }
-        else if (myAge.value > 65){
+
+        else if (myAge.value == 'over'){
             let discountPrice40 = (basePrice - (basePrice * 0.40)).toFixed(2);
             console.log('Prezzo scontato per gli over 65: €', discountPrice40);
+            offerType.innerHTML = 'Offerta Over 65 -40%';
+            ticketPrice.innerHTML = '€ ' + discountPrice40;
         }
-        else{
+
+        else if (myAge.value == 'adult'){
             console.log('Il prezzo del biglietto sarà: €', basePrice);
+            offerType.innerHTML = 'Prezzo Standard';
+            ticketPrice.innerHTML = '€ ' + basePrice;
         }
 }
 );
